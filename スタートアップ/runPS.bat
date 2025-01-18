1240620f1ffe9bb42306d485c0b2666245b98f94
coffeebreak-chicken <117181942+coffeebreak-chicken@users.noreply.github.com>
2025-01-18 21:26:44 +0900

    新規作成

---
+++ スタートアップ/runPS.bat
@@ -0,0 +1,5 @@
+@echo off
+rem パワーシェル
+powershell -WindowStyle Hidden -File C:\Users\Student\Documents\GitHub\BatteryDisplay\PowerShell\BatteryDisplay.ps1
+
+exit /B 0
---
+++ スタートアップ/startBatteryDisplayGlobal.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem GUI用のコンソールウィンドウを表示させないで実行する pythonw
+cd C:\Users\Student\Documents\BatteryDisplayPy\project_py
+pythonw battery_display.py
+
+exit /B 0
